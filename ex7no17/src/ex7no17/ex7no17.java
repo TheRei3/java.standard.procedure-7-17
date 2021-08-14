329b027b67b99520318983f0476fdf42c30213fd
admin <admin@DESKTOP-COIHK7E>
2021-08-15 07:11:22 +0900

    ex7no17

---
+++ ex7no17/src/ex7no17/ex7no17.java
@@ -0,0 +1,40 @@
+package ex7no17;
+
+public class ex7no17 {  //  362p
+	public static void main(String[] args) {
+		FireEngine fe = new FireEngine();
+		
+		if(fe instanceof FireEngine) {
+			System.out.println("This is a FireEngine instance.");
+		}
+		
+		if(fe instanceof Car) {
+			System.out.println("This is a Car instance.");
+		}
+		
+		if(fe instanceof Object) {
+			System.out.println("This is an Object instance.");
+		}
+		
+		System.out.println(fe.getClass().getName());
+	}
+
+}
+class Car{
+	String color;
+	int door;
+	
+	void drive() {
+		System.out.println("drive, Brrrr~");
+	}
+	
+	void stop() {
+		System.out.println("stop!!!");
+	}
+}
+
+class FireEngine extends Car{
+	void water() {
+		System.out.println("water!!!");
+	}
+}
